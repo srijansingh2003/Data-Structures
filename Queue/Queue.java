eb8dabd99e5e262f60b7fe2b68aeae319f711523
srijansingh <srijansingh82003@gmail.com>
2022-09-20 23:55:07 +0530

    Added enQueue funtion in Queue

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -20,10 +20,22 @@ class Queue {
     }
 
     // Checking whether Queue is full
-    public Boolean isFull() {
+    public Boolean isFull(int size) {
         if (front == 0 && rear == size - 1)
             return true;
         else
             return false;
+    }
+
+    // Inserting an element in a Queue
+    public void enQueue(int x) {
+        if (isFull(size)) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1)
+                front = 0;
+            rear++;
+            items[rear] = x;
+        }
     }
 }
